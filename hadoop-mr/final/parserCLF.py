8274301b8fb11b3638b19253e616b7baa8b5d80c
Angel <aj.abregu@yahoo.com.ar>
2013-11-18 02:10:03 -0500

    Agregado to-do-list

---
+++ hadoop-mr/final/parserCLF.py
@@ -0,0 +1,31 @@
+import re
+from urlparse import urlparse
+
+patternCLF = re.compile(r"""^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(-|\w+)\s+(-|\w+)\s+(\[.*\])\s+\"(.*)\"\s+(\d{3})\s+(-|\d+)\s*$""")
+patternReq = re.compile(r"""^[A-Z]+\s+(.*)\s+.*$""")
+
+
+def parseLine(s):
+	matches = patternCLF.match(s)
+	try:
+		res = matches.groups()
+	except:
+		res = None
+	return res
+	
+def getResourceName(s):
+	matches = patternReq.match(s)
+	try:
+		req = matches.groups()[0]
+		o = urlparse(req)
+		# According to udacity forum:
+		# res = o.path + o.params + o.query
+		res = o.path
+		if o.params:
+			res += ";" + o.params
+		if o.query:
+			res += "?" + o.query
+	except:
+		res = None
+	return res
+
---
+++ hadoop-mr/final/parserCLF_testcase.py
@@ -0,0 +1,42 @@
+import re
+import parserCLF
+import unittest
+
+
+
+class ParserCLFTestCase(unittest.TestCase):
+	
+	def test_parseLineMatches(self):
+		line = """10.223.157.186 - - [15/Jul/2009:15:50:35 -0700] "GET /displaytitle.php?id=347 HTTP/1.1" 200 10469"""
+		
+		expected_ip = "10.223.157.186"
+		expected_id = "-"
+		expected_uname = "-"
+		expected_timef = "[15/Jul/2009:15:50:35 -0700]"
+		expected_requestline = "GET /displaytitle.php?id=347 HTTP/1.1"
+		expected_statuscode = "200"
+		expected_size = "10469"
+		
+		res = parserCLF.parseLine(line)
+		
+		self.assertNotEqual(res,None)
+		self.assertEqual(expected_ip,res[0])
+		self.assertEqual(expected_id,res[1])
+		self.assertEqual(expected_uname,res[2])
+		self.assertEqual(expected_timef,res[3])
+		self.assertEqual(expected_requestline,res[4])
+		self.assertEqual(expected_statuscode,res[5])
+		self.assertEqual(expected_size,res[6])	
+		
+	def test_getResourceNameMatches(self):
+		req = "GET /displaytitle.php?id=347 HTTP/1.1"
+		
+		expected_resource = "/displaytitle.php?id=347"
+		res = parserCLF.getResourceName(req)
+		
+		self.assertNotEqual(res,None)
+		self.assertEqual(expected_resource,res)
+		
+
+if __name__ == '__main__':
+    unittest.main()
